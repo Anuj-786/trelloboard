21420561f0b551a29170ce963cf84e042d97d7d6
Anuj Thakur <anujthakur016@gmail.com>
2019-09-09 16:53:45 +0530

    Working Version: Adding styles

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -22,28 +22,44 @@ class Comments extends Component {
     addComment = (e) => {
       e.preventDefault()
       this.props.addComment(this.props.boardId, this.state.comment, this.props.taskId)
+      this.setState({comment: ''})
     }
     render() {
       console.log(this.props.comments)
       let comments = this.props.comments
       return (
         <div>
-          <span style={{fontSize: 12}}>Comments</span>
-          {
-            comments.allComments && comments.allComments.map( ({boardId, comment, taskId, commentId}) => {
-              return (
-                taskId === this.props.taskId &&
-                <div key={commentId}>
-                  <span style={{fontSize: 10}}>{comment}</span>
-                </div>  
-              ) 
-                
-            })
-          }
-          <form onSubmit={this.addComment}>
-            <input type="text" value={this.state.comment} onChange={this.onChangeComment} />
-            <button type="submit">Add Comments</button>
-          </form>
+          <div>
+            <form onSubmit={this.addComment}>
+              <input 
+                type="text" 
+                placeholder="add your comment" 
+                value={this.state.comment} 
+                onChange={this.onChangeComment} 
+                style={{border: '1px solid cornflowerblue', padding: 4, borderRadius: 20, outline: 'none'}}
+              />
+              <button 
+                type="submit"
+                style={{background:'cornflowerblue', color: '#fff', border: 'none', padding: '4px', borderRadius: 20, marginLeft: 5}}
+              >
+                Add Comment
+              </button>
+            </form>
+          </div>
+          <div style={{margin: 10, paddingBottom: 5}}>
+            <span style={{fontSize: 12, fontWeight: 600}}>Comments:</span>
+            {
+              comments.allComments && comments.allComments.map( ({boardId, comment, taskId, commentId}) => {
+                return (
+                  taskId === this.props.taskId &&
+                  <div key={commentId} style={{marginLeft: 5}}>
+                    <span style={{fontSize: 10}}>{comment}</span>
+                  </div>  
+                ) 
+                  
+              })
+            }
+          </div>
         </div>
       )
     }
--- src/components/CreateTask.js
+++ src/components/CreateTask.js
@@ -28,20 +28,31 @@ class CreateTask extends Component {
   }
   render () {
     return (
-      <div> 
+      <div style={{marginTop: 15}}> 
         <form
         onSubmit={this.addTask}
+        style={{textAlign: 'center'}}
         >
-          <input value={this.state.taskTitle} onChange={this.onChange} placeholder="create new task"></input>
-          <button  type="submit">Add Task</button>
+          <input
+            value={this.state.taskTitle} 
+            onChange={this.onChange} 
+            placeholder="create new task"
+            style={{padding: 10, borderRadius: 20, border: '1px solid cornflowerblue', outline: 'none'}}
+            >
+          </input>
+          <button  
+            type="submit"
+            style={{padding: 10, background: 'cornflowerblue', border: 'none', color: '#fff', fontWeight: 600, borderRadius: 20, marginLeft: 5}}
+          >
+          Add Task</button>
         </form>
-        <div style={{height: 200, overflow: 'scroll'}}>
+        <div style={{height: 200, overflow: 'scroll', marginLeft: 30}}>
          { 
            this.props.allTasks && this.props.allTasks.map( data => {
               return (
                 this.props.boardId == data.boardId ? 
                   <div key={data.taskId}>
-                      <li style={{listStyle: 'none'}}>{data.title}</li>
+                      <li style={{listStyle: 'none', marginBottom: 10}}>{data.title}</li>
                       <AddComments taskId={data.taskId} boardId={data.boardId} />                     
                   </div>
                   : ''
